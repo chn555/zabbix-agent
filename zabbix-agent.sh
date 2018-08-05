c18dfe4372aeed2916818da4404879d05f82f6e9
chn555 <chn566@gmail.com>
2018-08-05 16:55:10 +0300

    second

--- zabbix-agent.sh
+++ zabbix-agent.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+
+setenforce 0
+firewall-cmd --add-port=10050/tcp --permanent
+rpm -ivh https://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm
+
+ yum install zabbix-agent
