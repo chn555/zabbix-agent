99a0d8d967c55709ec1e32acddc83de5f59d21fe
chn555 <chn566@gmail.com>
2018-08-05 16:59:33 +0300

    fourth

--- zabbix-agent.sh
+++ zabbix-agent.sh
@@ -7,5 +7,5 @@ rpm -ivh https://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.e
 
  yum install zabbix-agent
 
- sed -i -e 's/# EnableRemoteCommands=0/EnableRemoteCommands=0/g' /etc/zabbix/zabbix_agent.conf
- sed -i -e 's/Server=127.0.0.1/Server=10.0.0.5/g' /etc/zabbix/zabbix_agent.conf
+ sed -i -e 's/# EnableRemoteCommands=0/EnableRemoteCommands=0/g' /etc/zabbix/zabbix_agentd.conf
+ sed -i -e 's/Server=127.0.0.1/Server=10.0.0.5/g' /etc/zabbix/zabbix_agentd.conf
